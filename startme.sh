d1e8d5b1c5ed542a26ded9cf2269819bc7a8da01
Jay Garces <jay.garces@gmail.com>
2020-07-23 08:00:44 -1000

    Delete startme.sh

--- startme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/pi/checkpointPlus_final
-git fetch
-git reset --hard origin/master
-python  /home/pi/checkpointPlus_final/checkpointRoutinev4.py >/tmp/checkpoint.log 2>/tmp/checkpoint.err
