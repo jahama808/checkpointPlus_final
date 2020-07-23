beee206576b890515cc603458d2337e6c47e3481
Jay Garces <jay.garces@gmail.com>
2020-07-23 09:39:21 -1000

    cleanup

--- target2.py
+++
@@ -1,8 +0,0 @@
-#ooklaTargets = [22494]
-
-#ooklaTargets = [22494,24031,10669]
-ooklaTargets = ["htspeed3.hawaiiantel.net"]
-#10669 was removed on 12/5/2019 - started seeing all the tests coming from there.
-
-
-#ooklaTargets = [24031,22494]
