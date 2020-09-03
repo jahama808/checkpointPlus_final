958eaff6152ecc800ce9a8f9ca45382f3461cd37
Jay Garces <jay.garces@gmail.com>
2020-09-02 14:16:28 -1000

    Update startme.sh
    added delay before running script to allow pi to settle before running first test.  Also added a dump to a .log and .err file in /tmp.

--- startme.sh
+++ startme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+sleep 20
 cd /home/pi/checkpointPlus_final
 git fetch
 git reset --hard origin/master
-python  /home/pi/checkpointPlus_final/checkpointRoutine.py
+python  /home/pi/checkpointPlus_final/checkpointRoutine.py >/tmp/checkpoint.log 2>/tmp/checkpoint.err
